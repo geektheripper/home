1be5529ca4db7fa0c82fb5ac64aca2b1a35c363f
GeekTR <geektheripper@gmail.com>
2021-03-28 20:45:10 +0800

    init docker

---
+++ profile/init/docker.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+latest_ver() {
+  curl --silent "https://api.github.com/repos/$1/releases/latest" |
+    grep '"tag_name":' |
+    sed -E 's/.*"([^"]+)".*/\1/'
+}
+
+set_aliyun_mirror() {
+  mkdir -p /etc/docker
+
+  echo '{"registry-mirrors": ["https://23deml29.mirror.aliyuncs.com"]}' | sudo tee /etc/docker/daemon.json
+
+  sudo systemctl daemon-reload
+  sudo systemctl restart docker
+}
+
+install_docker_compose() {
+  version=$(latest_ver docker/compose)
+
+  sudo curl -L "https://github.com/docker/compose/releases/download/${version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
+}
+
+if planetarian::net::in-gfw; then
+  curl -fsSL https://get.docker.com | bash -s docker --mirror Aliyun
+  set_aliyun_mirror
+else
+  curl -fsSL https://get.docker.com | bash -s docker
+fi
+
+if id geektr; then sudo usermod -aG docker geektr; fi
+if id yumemi; then sudo usermod -aG docker yumemi; fi
+
+planetarian::proxy::set
+install_docker_compose

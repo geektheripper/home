eca93f10d021f843a6102e6abc79c96bf7e150ef
GeekTR <geektheripper@gmail.com>
2021-04-03 04:49:54 +0800

    i secret init

--- profile/scripts/secret.sh
+++ profile/scripts/secret.sh
@@ -1,9 +1,22 @@
 #!/usr/bin/env bash
 
-planetarian::secret::init() {
-  curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
-  sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-  sudo apt-get update && sudo apt-get install vault
+planetarian::secret::init_drive() {
+  pltr_secret_base="/run/planetarian/secret"
+  if df -a | grep "$pltr_secret_base" >/dev/null; then
+    echo "$pltr_secret_base"
+    return 0
+  fi
+  sudo mkdir -p "$pltr_secret_base"
+  sudo mount -t ramfs ramfs "$pltr_secret_base"
+  sudo chown "$UID:$UID" "$pltr_secret_base"
+  sudo chmod go-rwx "$pltr_secret_base"
+  echo "$pltr_secret_base"
 }
+
+planetarian::secret::init() {
+  planetarian::feature_switch secret autoload on
+}
+
+planetarian::feature_switch secret autoload && planetarian::secret::init_drive >/dev/null
 
 pcmd "secret init" planetarian::secret::init

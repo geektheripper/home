c6cf3ed802f18be20f427b13ba9d3df5d2e84878
GeekTR <geektheripper@gmail.com>
2021-05-08 08:36:52 +0800

    init vault and packer

--- profile/scripts/secret.vault.sh
+++ profile/scripts/secret.vault.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
 planetarian::secret::vault::init() {
-  curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
-  sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-  sudo apt-get update && sudo apt-get install vault
+  planetarian::init vault
 }
 
 planetarian::secret::vault::clear() {

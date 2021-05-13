aeb3943535dbf9f8751fe785d49cf61ea003a9e6
GeekTR <geektheripper@gmail.com>
2021-05-14 05:26:56 +0800

    fix bug when init vault

--- profile/init/vault.sh
+++ profile/init/vault.sh
@@ -3,4 +3,4 @@
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 sudo apt-add-repository "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 
-sudo apt-get install -y packer
+sudo apt-get install -y vault

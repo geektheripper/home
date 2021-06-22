82e386ecaa30be6014a5ce9e2f9b4c24e949f45c
GeekTR <geektheripper@gmail.com>
2021-06-23 02:54:38 +0800

    add acme deplay target: scp

--- profile/scripts/acme.apply.sh
+++ profile/scripts/acme.apply.sh
@@ -12,6 +12,21 @@ planetarian::acme::apply::console() {
   cat "$base/$domain.key"
 }
 
+# i acme apply scp geektr.co yumemi@portainer.geektr.co /srv/portainer key.pem cert.pem
+planetarian::acme::apply::scp() {
+  base=$1
+  domain=$2
+  connect_str=$3
+  target_dir=$4
+  priv_key_name=$5
+  pub_key_name=$6
+
+  key="$HOME/.ssh/keys/geektr.co/root"
+
+  scp -i "$key" "$base/fullchain.cer" "$connect_str":"$target_dir"/"$pub_key_name"
+  scp -i "$key" "$base/$domain.key" "$connect_str":"$target_dir"/"$priv_key_name"
+}
+
 # scp "$HOME/.ssh/keys/geektr.co/root.pub" root@ryou.geektr.co:/etc/ssh/keys-root/authorized_keys
 planetarian::acme::apply::esxi() {
   base=$1

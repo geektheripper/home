f66b1503958253bfd0b9615099856207eb609b3d
GeekTR <geektheripper@gmail.com>
2021-11-13 03:51:13 +0800

    use proxy when install vault

--- profile/init/vault.sh
+++ profile/init/vault.sh
@@ -3,5 +3,15 @@
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 sudo apt-add-repository "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 
-sudo apt-get update
-sudo apt-get install -y vault
+papt() {
+  if planetarian::net::in-gfw; then
+    planetarian::proxy::set
+    # shellcheck disable=SC2154
+    sudo http_proxy="$http_proxy" https_proxy="$http_proxy" apt-get "$@"
+  else
+    sudo apt-get "$@"
+  fi
+}
+
+papt update
+papt install -y vault
